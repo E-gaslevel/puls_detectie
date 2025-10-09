e2291597c722fa9755ec234d3327a7b93122e872
Krzysztof Cajler <krisss0507@gmail.com>
2025-10-09 10:55:47 +0200

    Auto port selection, shows plot analog values
    auto port selections works for windows, linux/macos should work, yet to
    test
    Plot is shown in matplotlib

--- logger.py
+++ logger.py
@@ -1,10 +1,25 @@
 import serial
+import serial.tools.list_ports
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sys import exit
 import os
 
+data_number = []
 
+ports = list(serial.tools.list_ports.comports())
 try:
-    ser = serial.Serial('COM6', baudrate=9600, timeout=None, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=8)
+    for p in ports:
+        if 'CP210' in p.description:
+            ser = serial.Serial(p.device, 
+                                baudrate=9600, 
+                                timeout=None, 
+                                parity=serial.PARITY_NONE, 
+                                stopbits=serial.STOPBITS_ONE, 
+                                bytesize=8)
+            print(f"Connected to {p.description}")
 except Exception as e:
     print(e)
     exit("Couldn't connect!")
@@ -13,7 +28,11 @@ except Exception as e:
 line = (ser.readline().decode('ascii').strip())
 # Split received string by comma
 values = line.split(',')
-
+for v in values:
+    try:
+        data_number.append((int(v)/4095)*1.25)
+    except ValueError:
+        pass
 
 # Check for new filename, dont overwrite old reading file
 count = 0
@@ -26,5 +45,11 @@ while True:
 # Write to .txt
 with open(filename, "w") as file:
     file.write('\n'.join(values))
+    
+x = np.arange(0., 5000., 1)
+
+plt.axis([0, 5000, -0.1, 1.3])
+plt.plot(x, data_number)
+plt.show()
     
 
