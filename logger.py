6150762b9895c1f0c01cbbb4558327191b2578d9
Krzysztof Cajler <krisss0507@gmail.com>
2025-10-17 14:11:56 +0200

    Added bi-directional communication Program now sends parameters and receives data Stores in a file with a specific nameformat NOT tested

--- logger.py
+++ logger.py
@@ -1,63 +1,64 @@
 import serial
 import serial.tools.list_ports
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 from sys import exit
 import os
 import string
 
-data_number = []
 
-ports = list(serial.tools.list_ports.comports())
-try:
-    for p in ports:
-        if 'CP210' in p.description:
-            ser = serial.Serial(p.device, 
-                                baudrate=115200, 
-                                timeout=None, 
-                                parity=serial.PARITY_NONE, 
-                                stopbits=serial.STOPBITS_ONE, 
-                                bytesize=8)
-            print(f"Connected to {p.description}")
-except Exception as e:
-    print(e)
-    exit("Couldn't connect!")
-
-ans = input("Do you want to to receive data? Press y/n ")
-if ans == "y":
+def connect_UART():
+    ports = list(serial.tools.list_ports.comports())
+    try:
+        for p in ports:
+            if 'CP210' in p.description:
+                ser = serial.Serial(p.device, 
+                                    baudrate=115200, 
+                                    timeout=None, 
+                                    parity=serial.PARITY_NONE, 
+                                    stopbits=serial.STOPBITS_ONE, 
+                                    bytesize=8)
+                print(f"Connected to {p.description}")
+                return ser 
+    except Exception as e:
+        print(e)
+        exit("Couldn't connect!")
+        
+# This function sends over UART the sample parameters, C program will read those and assign those value
+# Filename is also msg to uC
+def send_parameter_UART(ser, sample):
+    filename = f"f{sample[0]}d{sample[1]}n{sample[2]}\n"
+    print(f"File name will be {filename}")
+    ser.write(filename.encode())
+    return filename
     
+def read_UART_and_save(ser, _filename):
     #Read UART
     line = (ser.readline().decode('ascii').strip())
     line = ''.join(ch for ch in line if ch in string.printable)
     # Split received string by comma
     values = line.split(',')
-    # Append values to show it in plt later
-    for v in values:
-        try:
-            data_number.append((int(v)/4095)*1.25)
-        except ValueError:
-            pass
 
-    # Check for new filename, dont overwrite old reading file
+    # Check for new filename, dont overwrite old reading file, add index_number to filename
     count = 0
+    filename = _filename + "_" + str(count)
     while True:
-        filename = os.path.join("readings", f"reading{count}.txt")
-        if not os.path.exists(filename):
+        if os.path.exists(filename):
+            count += 1
+            filename = filename + "_" + count
+        else:
+            file_to_save = os.path.join("readings", f"{filename}.txt")
             break
-        count += 1
+        
 
     # Write to .txt
-    with open(filename, "w") as file:
+    with open(file_to_save, "w") as file:
         file.write('\n'.join(values))
-else:
-    exit()
     
-# x = np.arange(0., 5000., 1)
-
-# plt.axis([0, 5000, -0.1, 1.3])
-# plt.plot(x, data_number)
-# plt.show()
+if __name__ == "__main__":
+    ser = connect_UART()
     
-
+    # Voer hier sample parameters, every list is a new measurement 
+    parameters = [[50000, 100, 2], [20000, 50, 1]]
+    for p in parameters:
+        filename = send_parameter_UART(ser, p)
+        read_UART_and_save(ser, filename)
